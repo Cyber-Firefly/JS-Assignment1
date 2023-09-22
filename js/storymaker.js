658e6d900168ebdd26eecac1304bf5458223d956
John.Gu <john.gu@yahoo.com>
2023-09-22 10:46:22 -0400

    added js up until viriables for count

--- js/storymaker.js
+++ js/storymaker.js
@@ -3,19 +3,32 @@
 /* Variables
 -------------------------------------------------- */
 // Constants for main button query selectors
-
+const noun1 = document.querySelector("#noun1");
+const verb = document.querySelector("#verb");
+const adjective = document.querySelector("#adjective");
+const noun2 = document.querySelector("#noun2");
+const setting = document.querySelector("#setting");
 
 // Constants for p tag to display query selectors
-
+const chooseNoun1 = document.querySelector("#chooseNoun1");
+const chooseVerb = document.querySelector("#chooseVerb");
+const chooseAdjective = document.querySelector("#chooseAdjective");
+const chooseNoun2 = document.querySelector("#chooseNoun2");
+const chooseSetting = document.querySelector("#chooseSetting");
 
 // Constants for final buttons and p tags
-
+const showStory = document.querySelector("#playback");
+const randomStory = document.querySelector("#random");
 
 // Variables for pre-defined arrays
-
+var noun1Array = ["Turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"];
+var verbArray = ["sat on", "ate", "danced with", "saw", "doesn't like", "kissed with"];
+var adjectiveArray = ["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"];
+var noun2Array = ["goat", "monkey", "fish", "cow", "frog", "bug", "worm"];
+var settingArray = ["on the moon", "on the chair", "in my spaghetti", "in my soup", "on the grass", "in my shoes"];
 
 // Variables for count to grab array elements
-
+var count;
 
 /* Functions
 -------------------------------------------------- */
